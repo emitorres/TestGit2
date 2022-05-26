45a92955914695ab4e3311a77100293ac7950265
emitorres <emitorres93@gmail.com>
2022-05-26 16:50:00 -0300

    Agregado GET y SET

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,20 @@
 public class Test {
     private String Nombre;
     private String Apellido;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
 }
