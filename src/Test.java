2f7f0dbea696fec46b9b15671d71d791c9c7223a
emitorres <emitorres93@gmail.com>
2022-05-26 16:59:52 -0300

    Agregando nuevo atributo

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,29 @@
 public class Test {
-    private String nombre;
-    private String apellido;
-    private String direccion;
+    private String Nombre;
+    private String Apellido;
+    private String Direccion;
 
     public String getNombre() {
-        return nombre;
+        return Nombre;
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        Nombre = nombre;
     }
 
     public String getApellido() {
-        return apellido;
+        return Apellido;
     }
 
     public void setApellido(String apellido) {
-        this.apellido = apellido;
+        Apellido = apellido;
     }
 
     public String getDireccion() {
-        return direccion;
+        return Direccion;
     }
 
     public void setDireccion(String direccion) {
-        this.direccion = direccion;
+        Direccion = direccion;
     }
 }
