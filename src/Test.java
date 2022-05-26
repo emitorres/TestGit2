7f0987bb3049912cd6fcabe9881845b9bf469ee0
emitorres <emitorres93@gmail.com>
2022-05-26 18:11:12 -0300

    a

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,8 @@ public class Test {
     private String apellido;
     private String direccion;
     private String cuil;
-    private String dni1;
-    private String edad1;
+    private String dni;
+    private String edad;
     private String algo;
-    private String tele;
+    private String telefono;
 }
