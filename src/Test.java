216ef3fba754de4489e145dbf29ec8a0e0c59e91
emitorres <emitorres93@gmail.com>
2022-05-26 18:24:27 -0300

    Cambios

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,6 @@ public class Test {
     private String cuil;
     private String dni;
     private String edad;
-    private String algo1;
+    private String algoModificado;
     private String celular;
 }
