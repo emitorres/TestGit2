2d8d123cbee81f522d122cd24b650450175761d1
emitorres <emitorres93@gmail.com>
2022-05-26 17:15:34 -0300

    Eliminando get y set

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,7 @@
 public class Test {
-    private String Nombre;
-    private String Apellido;
-    private String Direccion;
+    private String nombre;
+    private String apellido;
+    private String direccion;
 
-    public String getNombre() {
-        return Nombre;
-    }
 
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-
-    public String getApellido() {
-        return Apellido;
-    }
-
-    public void setApellido(String apellido) {
-        Apellido = apellido;
-    }
-
-    public String getDireccion() {
-        return Direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        Direccion = direccion;
-    }
 }
